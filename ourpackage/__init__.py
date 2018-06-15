e0aa7642d2d140ad6aa2c14a7fd5cd52a944b659
Jake <jakemacnaughton@gmail.com>
2018-06-15 12:01:29 -0400

    removed solution

--- ourpackage/__init__.py
+++ ourpackage/__init__.py
@@ -2,20 +2,18 @@
 from flask import Flask
 # import SQLAlchemy from flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-# import dash
-import dash
 
 
 # initialize new flask app
-server = Flask(__name__)
+app = Flask(__name__)
 # add configurations and database
-server.config['DEBUG'] = True
-server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # connect flask_sqlalchemy to the configured flask app and create the Dash app
-db = SQLAlchemy(server)
-app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard')
+
+
 
 #import our routes after our database has been configured
 from ourpackage import routes
